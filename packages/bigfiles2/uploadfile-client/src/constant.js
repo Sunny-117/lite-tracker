124abc465003e858c0f1a477cd3e0334f5915995
Sunny-117 <zhiqiangfu6@gmail.com>
2024-05-25 18:48:36 +0800

    chore: cake

--- packages/bigfiles2/uploadfile-client/src/constant.js
+++
@@ -1,7 +0,0 @@
-export const MAX_FILE_SIZE = 2 * 1024 * 1024 * 1024;
-export const CHUNK_SIZE = 100 * 1024 * 1024;
-export const UploadStatus = {
-  NOT_STARTED: "NOT_STARTED",
-  UPLOADING: "UPLOADING",
-  PAUSED: "PAUSED",
-};
