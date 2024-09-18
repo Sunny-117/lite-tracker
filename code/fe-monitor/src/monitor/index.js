535f2b2acfa3204826322dab9d5267b64741d9d4
fuzhiqiang <fuzhiqiang@baidu.com>
2024-09-18 18:08:47 +0800

    code

--- code/fe-monitor/src/monitor/index.js
+++
@@ -1,6 +0,0 @@
-import { blankScreen } from './lib/blankScreen';
-import { injectJsError } from './lib/jsError';
-import { injectXHR } from './lib/xhr';
-injectJsError();
-injectXHR()
-blankScreen() // 因为这段代码是插入到head里面，所以白屏会一直上报，所以可以放到onload.js里面
