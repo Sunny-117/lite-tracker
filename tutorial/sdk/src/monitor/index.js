1e32e163ad1930d804cbfad79393e4f9c2ea6d1f
Sunny-117 <zhiqiangfu6@gmail.com>
2025-07-23 17:26:04 +0800

    feat: pv

--- tutorial/sdk/src/monitor/index.js
+++ tutorial/sdk/src/monitor/index.js
@@ -3,9 +3,11 @@ import { injectXHR } from './lib/xhr'
 import { blankScreen } from './lib/blankScreen'
 import { timing } from './lib/timing'
 import {longTask} from './lib/longTask'
+import {pv} from './lib/pv'
 
 injectJsError()
 injectXHR()
 blankScreen()
 timing()
 longTask()
+pv()
